a6c20f7b0ebcc7ffe38df5c5e80f97c0653386c9
FeltB00T <olegmed09@gmail.com>
2026-05-16 23:34:39 +0500

    winxp_речь-08.1
    хронологически вроде перед 08.1, о чём и говорит минус

--- речь/main.asm
+++ речь/main.asm
@@ -6,12 +6,11 @@ extern rt_init
 extern rt_error_string
 extern platform_init
 extern platform_read_file
-extern platform_last_error
-extern platform_last_stage
 
 extern _GetCommandLineW@0
 extern _CommandLineToArgvW@8
 extern _LocalFree@4
+extern _GetFullPathNameW@16
 extern _ExitProcess@4
 
 global _start
@@ -20,19 +19,13 @@ section .data
 msg_usage db "main.exe <имя файла>"
 msg_usage_len equ $ - msg_usage
 
-msg_open_fail db "Ошибка: не удалось открыть/прочитать файл. stage="
+msg_open_fail db "Ошибка: не удалось открыть файл."
 msg_open_fail_len equ $ - msg_open_fail
 
-msg_err db " Win32 code="
-msg_err_len equ $ - msg_err
-
-msg_nl db 13, 10
-msg_nl_len equ $ - msg_nl
-
 section .bss
+buffer      resb 1048576
+fullpathbuf resw 1024
 argc        resd 1
-input_ptr   resd 1
-numbuf      resb 16
 
 section .text
 
@@ -50,44 +43,50 @@ _start:
     test eax, eax
     jz .usage
 
-    mov ebx, eax
+    mov ebx, eax              ; argv array pointer
     mov ecx, [argc]
     cmp ecx, 2
     jb .free_and_usage
 
-    mov esi, [ebx+4]              ; argv[1]
+    mov esi, [ebx+4]          ; argv[1]
 
+    push dword 0
+    push dword fullpathbuf
+    push dword 1024
     push esi
+    call _GetFullPathNameW@16
+    test eax, eax
+    jz .free_and_openfail
+    cmp eax, 1024
+    jae .free_and_openfail
+
+    push dword 1048576
+    push dword buffer
+    push dword fullpathbuf
     call platform_read_file
-    add esp, 4
+    add esp, 12
 
     cmp eax, -1
     je .free_and_openfail
 
-    mov esi, eax                  ; ESI = buffer with file contents
+    mov eax, buffer
 
-    cmp byte [esi], 0EFh
-    jne .store_ptr
-    cmp byte [esi+1], 0BBh
-    jne .store_ptr
-    cmp byte [esi+2], 0BFh
-    jne .store_ptr
-    add esi, 3
+    cmp byte [eax], 0EFh
+    jne .init_cur
+    cmp byte [eax+1], 0BBh
+    jne .init_cur
+    cmp byte [eax+2], 0BFh
+    jne .init_cur
+    add eax, 3
 
-.store_ptr:
-    mov [input_ptr], esi
-
-    mov eax, esi
+.init_cur:
     call cur_init
-
-    mov eax, [input_ptr]
     call parser_run
-    mov edi, eax
 
     push ebx
     call _LocalFree@4
 
-    push edi
+    push eax
     call _ExitProcess@4
 
 .free_and_usage:
@@ -107,73 +106,11 @@ _start:
     push ebx
     call _LocalFree@4
 
+.open_fail:
     push dword msg_open_fail_len
     push dword msg_open_fail
     call rt_error_string
     add esp, 8
 
-    mov eax, [platform_last_stage]
-    call u32_to_dec
-    push ecx
-    push eax
-    call rt_error_string
-    add esp, 8
-
-    push dword msg_err_len
-    push dword msg_err
-    call rt_error_string
-    add esp, 8
-
-    mov eax, [platform_last_error]
-    call u32_to_dec
-    push ecx
-    push eax
-    call rt_error_string
-    add esp, 8
-
-    push dword msg_nl_len
-    push dword msg_nl
-    call rt_error_string
-    add esp, 8
-
     push dword 1
     call _ExitProcess@4
-
-; eax = unsigned int
-; returns:
-;   eax = pointer to first digit
-;   ecx = length
-u32_to_dec:
-    push ebx
-    push edx
-    push edi
-
-    lea edi, [numbuf + 15]
-    mov byte [edi], 0
-    xor ecx, ecx
-    mov ebx, 10
-
-    cmp eax, 0
-    jne .loop
-
-    dec edi
-    mov byte [edi], '0'
-    mov ecx, 1
-    jmp .done
-
-.loop:
-    xor edx, edx
-    div ebx
-    add dl, '0'
-    dec edi
-    mov [edi], dl
-    inc ecx
-    test eax, eax
-    jnz .loop
-
-.done:
-    mov eax, edi
-    pop edi
-    pop edx
-    pop ebx
-    ret
--- речь/platform.asm
+++ речь/platform.asm
@@ -5,16 +5,9 @@ extern _ReadFile@20
 extern _CloseHandle@4
 extern _SetConsoleCP@4
 extern _SetConsoleOutputCP@4
-extern _GetLastError@0
-extern _GetProcessHeap@0
-extern _HeapAlloc@12
-extern _HeapFree@12
-extern _GetFileSize@8
 
 global platform_init
 global platform_read_file
-global platform_last_error
-global platform_last_stage
 
 %define GENERIC_READ          80000000h
 %define FILE_SHARE_READ       1
@@ -23,29 +16,24 @@ global platform_last_stage
 %define OPEN_EXISTING         3
 %define FILE_ATTRIBUTE_NORMAL  80h
 %define CP_UTF8               65001
-%define HEAP_ZERO_MEMORY      8
 
 section .bss
-platform_last_error resd 1
-platform_last_stage  resd 1
-bytes_read           resd 1
-file_size            resd 1
-heap_handle          resd 1
+bytes_read resd 1
 
 section .text
 
 platform_init:
-    ;push dword CP_UTF8
-    ;call _SetConsoleCP@4
+    push dword CP_UTF8
+    call _SetConsoleCP@4
 
-    ;push dword CP_UTF8
-    ;call _SetConsoleOutputCP@4
+    push dword CP_UTF8
+    call _SetConsoleOutputCP@4
     ret
 
 ; cdecl:
-;   platform_read_file(path_wptr)
+;   platform_read_file(path_wptr, buffer_ptr, max_len)
 ; returns:
-;   EAX = pointer to loaded zero-terminated buffer
+;   EAX = bytes_read
 ;   EAX = -1 on failure
 platform_read_file:
     push ebp
@@ -54,113 +42,50 @@ platform_read_file:
     push esi
     push edi
 
-    mov dword [platform_last_error], 0
-    mov dword [platform_last_stage], 0
-    mov dword [bytes_read], 0
-    mov dword [file_size], 0
-    mov dword [heap_handle], 0
-
     mov esi, [ebp+8]      ; path (WCHAR*)
+    mov edi, [ebp+12]     ; buffer
+    mov ecx, [ebp+16]     ; max_len
 
-    mov dword [platform_last_stage], 1
+    cmp ecx, 1
+    jbe .fail
+    dec ecx               ; reserve 1 byte for zero terminator
 
     push dword 0
     push dword FILE_ATTRIBUTE_NORMAL
     push dword OPEN_EXISTING
     push dword 0
-    mov eax, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE
-    push eax
+    push dword FILE_SHARE_READ
+    or   dword [esp], FILE_SHARE_WRITE | FILE_SHARE_DELETE
     push dword GENERIC_READ
     push esi
     call _CreateFileW@28
 
     cmp eax, -1
-    jne .got_handle
+    je .fail
 
-    call _GetLastError@0
-    mov [platform_last_error], eax
-    jmp .fail
-
-.got_handle:
     mov ebx, eax
-
-    push dword 0
-    push ebx
-    call _GetFileSize@8
-
-    cmp eax, 0FFFFFFFFh
-    jne .size_ok
-
-    call _GetLastError@0
-    test eax, eax
-    jnz .close_fail
-
-.size_ok:
-    mov [file_size], eax
-
-    mov ecx, eax
-    inc ecx                    ; +1 for zero terminator
-
-    call _GetProcessHeap@0
-    test eax, eax
-    jz .close_fail
-
-    mov [heap_handle], eax
-
-    push ecx                   ; bytes
-    push dword 0               ; flags
-    push eax                   ; heap handle
-    call _HeapAlloc@12
-    test eax, eax
-    jz .close_fail
-
-    mov edi, eax               ; buffer pointer
-
-    mov dword [platform_last_stage], 2
 
     push dword 0
     lea  eax, [bytes_read]
     push eax
-    mov eax, [file_size]
-    push eax
+    push ecx
     push edi
     push ebx
     call _ReadFile@20
 
     test eax, eax
-    jnz .read_ok
+    jz .close_fail
 
-    call _GetLastError@0
-    mov [platform_last_error], eax
-
-    mov eax, [heap_handle]
-    test eax, eax
-    jz .close_fail_only
-
-    push edi
-    push dword 0
-    push eax
-    call _HeapFree@12
-
-.close_fail_only:
-    push ebx
-    call _CloseHandle@4
-    jmp .fail
-
-.read_ok:
     mov eax, [bytes_read]
     mov byte [edi+eax], 0
 
     push ebx
     call _CloseHandle@4
 
-    mov eax, edi
+    mov eax, [bytes_read]
     jmp .done
 
 .close_fail:
-    call _GetLastError@0
-    mov [platform_last_error], eax
-
     push ebx
     call _CloseHandle@4
 
